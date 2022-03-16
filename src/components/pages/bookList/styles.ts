d5ebd30eb9476372c6dc4412bd61a5ea0f4c25c2
Rickson Oliveira <rickson.patrick298@gmail.com>
2022-03-16 13:18:48 -0300

    Fix scroll x

--- src/components/pages/bookList/styles.ts
+++ src/components/pages/bookList/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-width: 100vw;
+  min-width: 100%;
   min-height: 100vh;
   padding: 35px;
   background: #f7f7f7;
